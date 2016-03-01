2bdf1c9c1b6f9774bc381d6ae69806a68b77b417
zeusyu2013 <707073900@qq.com>
2016-03-01 22:56:57 +0800

    fix

--- Assets/Script/Logic/IObject.cs
+++ Assets/Script/Logic/IObject.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class IObject
 {
+    /// <summary>
+    /// 对象属性
+    /// </summary>
     public GameObject mObjGo;
     public Transform mObjTrans;
 
+    /// <summary>
+    /// 对象唯一标示
+    /// </summary>
     public int mObjId;
 
+    /// <summary>
+    /// 销毁
+    /// </summary>
     public void DestroySelf()
     {
         mObjGo = null;
